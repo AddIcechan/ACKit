d12e52fd4458f761ffba4506ce4581eb31b67d6a
ADDICE <addice.ch@gmail.com>
2017-12-18 16:45:56 +0800

    feat：加入 NSString + Extension

---
+++ ACKit/ACKit/Categories/NSString/NSString+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Extension.h
+//  ACKit
+//
+//  Created by ADDICE on 2017/12/18.
+//  Copyright © 2017年 ADDICE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Extension)
+
+@end
