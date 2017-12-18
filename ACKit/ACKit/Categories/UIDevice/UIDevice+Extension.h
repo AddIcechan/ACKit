a9e281a0920451971861965ad71ba685ee637329
ADDICE <addice.ch@gmail.com>
2017-12-18 16:22:30 +0800

    feat: 增加 UIDevice + extension , iPhoneX 判断方法

---
+++ ACKit/ACKit/Categories/UIDevice/UIDevice+Extension.h
@@ -0,0 +1,15 @@
+//
+//  UIDevice+Extension.h
+//  ACKit
+//
+//  Created by ADDICE on 2017/12/18.
+//  Copyright © 2017年 ADDICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+UIKIT_EXTERN inline BOOL iPhoneX(void);
+
+@interface UIDevice (Extension)
+
+@end
---
+++ ACKit/ACKit/Categories/UIDevice/UIDevice+Extension.m
@@ -0,0 +1,23 @@
+//
+//  UIDevice+Extension.m
+//  ACKit
+//
+//  Created by ADDICE on 2017/12/18.
+//  Copyright © 2017年 ADDICE. All rights reserved.
+//
+
+#import "UIDevice+Extension.h"
+
+BOOL iPhoneX(void) {
+    static BOOL result;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        CGSize size = [UIScreen mainScreen].bounds.size;
+        result = MAX(size.width, size.height) == 812;
+    });
+    return result;
+}
+
+@implementation UIDevice (Extension)
+
+@end
